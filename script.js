4affdaa698ac1cb13bfe969003fbebf71507817b
abdinasirhassan360-web <abdinasirhassan360@gmail.com>
2026-01-17 20:02:30 +0300

    adding floating effects

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const navLinks = document.getElementById("navLinks");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+const floatItems = document.querySelectorAll(".float-item");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  { threshold: 0.2 }
+);
+
+floatItems.forEach(item => observer.observe(item));
